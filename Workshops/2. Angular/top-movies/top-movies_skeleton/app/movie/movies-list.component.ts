54f92c67d2efc5b415edd371776b96af0d265374
Iasen Drenski <iasen.drenski@ingbank.com>
2016-12-23 12:21:23 +0200

    Implemented ng2-bootstrap

--- Workshops/2. Angular/top-movies/top-movies_skeleton/app/movie/movies-list.component.ts
+++ Workshops/2. Angular/top-movies/top-movies_skeleton/app/movie/movies-list.component.ts
@@ -3,34 +3,7 @@ import {Http} from '@angular/http';
 
 @Component({
     selector: 'mvdb-app',
-    templateUrl: 'movies-list.component.html',
-    styles: [`  
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
-    div {
-        padding-left: 15%;
-        padding-right: 15%;
-        padding-top: 5%;
-    }
-    th, tr {
-        
-        padding: 15px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-    }
-    td {
-        
-        padding: 15px;
-        text-align: left;
-    }
-    tr:hover{background-color:#f5f5f5}
-    tr.head {
-    border-bottom: 0px;
-    }   
-
-`]
+    templateUrl: 'movies-list.component.html'
 })
 export class MovieListComponent implements OnInit {
     movies: Array<any>;
